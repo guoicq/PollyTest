93c97aca8186d5d3655f2a8a1b56fd3ddb8fd571
Charles Guo <guoicq@gmail.com>
2021-04-25 12:17:08 -0400

    Implement HTTP call retries with exponential backoff with IHttpClientFactory

---
+++ PollyTest/PollyTest/ApiClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Linq;
+using System.Collections;
+using System.Threading.Tasks;
+
+namespace PollyTest
+{
+    public class ApiClient: IApiClient
+    {
+        private readonly HttpClient httpClient;
+        public ApiClient(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<string> GetPost()
+        {
+            var response = await httpClient.GetAsync("random_joke");
+            response.EnsureSuccessStatusCode();
+            var result = await response.Content.ReadAsStringAsync();
+            return result;
+        }
+    }
+
+}
---
+++ PollyTest/PollyTest/IApiClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net.Http;
+using System.Linq;
+using System.Collections;
+using System.Threading.Tasks;
+
+namespace PollyTest
+{
+    public interface IApiClient
+    {
+        Task<string> GetPost();
+    }
+
+}
